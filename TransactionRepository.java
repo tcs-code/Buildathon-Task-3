34962af9dbfb7ecf1e098602d593f27aecd96ed8
Rahul V <107417337+tcs-code@users.noreply.github.com>
2025-02-26 14:14:15 +0530

    Delete TransactionRepository.java

--- TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.personalfinancemanagement.personalfinancemanagement.Repository;
-
-import com.personalfinancemanagement.personalfinancemanagement.Models.Transactions;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transactions, Long> {
-}
