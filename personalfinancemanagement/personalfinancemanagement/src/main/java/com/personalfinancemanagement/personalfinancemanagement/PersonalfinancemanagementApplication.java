93d7c91a608bf16144eb2959558d87749106b20b
Rahul V <107417337+tcs-code@users.noreply.github.com>
2025-02-26 14:15:09 +0530

    Add files via upload

---
+++ personalfinancemanagement/personalfinancemanagement/src/main/java/com/personalfinancemanagement/personalfinancemanagement/PersonalfinancemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.personalfinancemanagement.personalfinancemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalfinancemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalfinancemanagementApplication.class, args);
+	}
+
+}
